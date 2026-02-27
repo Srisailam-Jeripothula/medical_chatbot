e3a11a9904e3d8702c8606d042e0854ac7cb3170
Srisailam Jeripothula <34628942+Srisailam-Jeripothula@users.noreply.github.com>
2026-02-27 11:23:24 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(name='medical_chatbot',
+      version='0.1',
+      description='A medical chatbot application',
+      author='Srisailam Jeripothula',
+      
+      packages=find_packages(),
+      install_requires=[],
+)
